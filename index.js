aac232fb9bdddf5fa2b2d2635aa58b81d53b907e
Ben Bruscella <benbruscella@gmail.com>
2018-01-09 18:37:18 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const ccxt = require ('ccxt');
+const _ = require ('lodash');
+
+(async function () {
+
+  let btcmarkets         = new ccxt.btcmarkets ()
+  let acx                = new ccxt.acx ()
+  let independentreserve = new ccxt.independentreserve ()
+
+  setInterval(async function () {
+
+    let btcmarkets_ticker         = await btcmarkets.fetch_ticker('BTC/AUD');
+    let acx_ticker                = await acx.fetch_ticker('BTC/AUD'  );
+    let independentreserve_ticker = await independentreserve.fetch_ticker('BTC/AUD');
+
+    let data = {
+      bid: [
+        btcmarkets_ticker.bid,
+        acx_ticker.bid,
+        independentreserve_ticker.bid
+      ],
+      ask: [
+        btcmarkets_ticker.ask,
+        acx_ticker.ask,
+        independentreserve_ticker.ask
+      ]
+    }
+    // console.log(data);
+    // console.log('max bid', _.max(data.bid));
+    // console.log('min ask', _.min(data.ask));
+    console.log('trade', (_.max(data.bid) - _.min(data.ask))/ _.min(data.ask) * 100);
+  }, 1000);
+
+}) ();
